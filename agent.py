720405ddfb3e94c8bc892db775b8a5640961a269
JonnyBP <jpbrasales@gmail.com>
2026-07-13 20:12:45 +0200

    feat: add structure for agent tools

---
+++ agent.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def setup_tools(self):
+        pass
+
+    def list_files_in_dir(self,directory="."):
+        pass
+
+    def read_file(self, path):
+        pass
+
+    def edit_file(self, path, prev_text, new_text):
+        pass
+
+
+    def process_response(self, response):
+        pass
